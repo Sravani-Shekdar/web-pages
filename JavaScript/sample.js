88cc146b05538a5e247e4dbd7592a36d85b44d8d
Sravani-Shekdar <sravanishekdar@gmail.com>
2024-07-22 23:23:17 +0530

    First Commit

---
+++ JavaScript/sample.js
@@ -0,0 +1,14 @@
+
+//     break;
+//     case 3:console.log("tuesday");
+//     break;
+//     case 4:console.log("wednesday");
+//     break;
+//     case 5:console.log("thursday");
+//     break;
+//     case 6:console.log("friday");
+//     break;
+//     case 7:console.log("saturday");
+//     break;
+    
+// }
---
+++ JavaScript/sravani.js
@@ -0,0 +1,109 @@
+// pop up box
+// var x =prompt("enter your name");
+// console.log(x);
+// console.log(typeof x);
+
+// var x= Number(prompt("enter first number"));
+// var y= Number(prompt("enter second number"));
+// console.log(x+y);
+// console.log(x-y);
+
+
+// var sra = confirm("do youwant to continue?");
+// console.log(sra);
+
+
+// string methods
+// 1 length
+// index 0
+// var x="Hello World";
+// x=x.toUpperCase()
+// console.log(x.length);
+// console.log(x.toUpperCase());
+// console.log(x.toLowerCase());
+
+
+
+// var x=prompt("enter the day");
+// switch(x.toLowerCase())
+// {
+//     case "monday": console.log("day1");
+//     break;
+//     case "tuesday": console.log("day2");
+//     break;
+//     case "wednesday": console.log("day3");
+//     break;
+//     case "thursday": console.log("day4");
+//     break;
+//     case "friday": console.log("day5");
+//     break;
+//     case "saturday": console.log("day6");
+//     break;
+//     case "sunday": console.log("day7");
+//     break;
+//     default:console.log("error");
+// }
+
+
+// var x= "hello world";
+// console.log(x.charAt(1));
+// console.log(x.charCodeAt(0));
+// console.log(x.split(""));
+
+// var x = new Date();
+// console.log(x);
+// // date and time
+// console.log(x.toLocaleString());
+// // time
+// console.log(x.toLocaleTimeString());
+// // date
+// console.log(x.toLocaleDateString());
+// console.log(x.getDate());
+// // 1-7
+// console.log(x.getDay());
+// // 0-11
+// console.log(x.getMonth());
+// console.log(x.getFullYear());
+// console.log(x.getHours());
+// console.log(x.getMinutes());
+// console.log(x.getSeconds());
+// console.log(DateMonth());
+
+
+// console.log(Math.sqrt(8)); 
+
+
+// var x= new Date(2022,0,23,13)
+// console.log(x);
+
+
+    // var x= new Date(2022,0,23,19) 
+    // var hour= x.getHours();
+
+    // if (hour < 12)
+    // {
+    //     console.log("Good morning!");
+    // } 
+    // else if (hour>12&&hour<17) 
+    // {
+    //     console.log("Good afternoon!");
+    // } 
+    // else
+    // {
+    //     console.log("Good evening!");   
+    // }
+
+
+// array
+// {}
+// homo-same datatype
+// hetero-different datatype
+// homogenous
+// var fruits=["apple","orange","kiwi"]
+// console.log(fruits);
+// heterogenous
+// var x = [1,2,3,4,nan,null]
+
+var fruits=["apple","orange","kiwi"]
+ console.log(fruits[2]);
+ console.log(fruits.length-1);
